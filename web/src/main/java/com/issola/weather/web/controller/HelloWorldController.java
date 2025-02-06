8f6c8eed16706ff2372a2e3abffb97356a092b05
cenkerozkan <ozkan.cenker@student.atilim.edu.tr>
2025-02-06 16:11:24 +0300

    Add MongoDB config and introduce Docker Compose setup
    Added MongoDB connection properties to `application.properties` for local development. Introduced a `docker-compose.yaml` file to orchestrate MongoDB, RabbitMQ, PostgreSQL, Loki, and Grafana services for the project. Removed unused imports in `HelloWorldController` for code cleanup.

--- web/src/main/java/com/issola/weather/web/controller/HelloWorldController.java
+++ web/src/main/java/com/issola/weather/web/controller/HelloWorldController.java
@@ -1,13 +1,10 @@
 package com.issola.weather.web.controller;
 
-import com.fasterxml.jackson.databind.util.JSONPObject;
-import org.apache.coyote.Response;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
-
 
 import java.util.HashMap;
 import java.util.Map;
