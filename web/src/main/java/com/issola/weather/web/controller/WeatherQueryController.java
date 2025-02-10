0b26ff2d85c2580c0cd83e375814efe1ad9f6bf1
cenkerozkan <ozkan.cenker@student.atilim.edu.tr>
2025-02-10 14:21:12 +0300

    Implemented mock endpoints for testing purposes in WeatherQueryController.java
    Added init.js in root for an entry point in docker to insert city data into MongoDB

--- web/src/main/java/com/issola/weather/web/controller/WeatherQueryController.java
+++ web/src/main/java/com/issola/weather/web/controller/WeatherQueryController.java
@@ -2,16 +2,32 @@ package com.issola.weather.web.controller;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
-import org.springframework.web.bind.annotation.RestController;
 
 import java.util.Map;
 import java.util.HashMap;
 
 @RestController
+@RequestMapping("/api/weather")
 public class WeatherQueryController
 {
-    @GetMapping("api/GetWeatherQuality")
+    @GetMapping("/QueryWeather")
+    public ResponseEntity<Map<String, String>> queryWeather()
+    {
+        Map<String, String> response = new HashMap<>();
+        response.put("message", "Query Weather!");
+        return new ResponseEntity<>(response, HttpStatus.OK);
+    }
+
+    @DeleteMapping("/DeleteRecord")
+    public ResponseEntity<Map<String, String>> deleteRecord()
+    {
+        Map<String, String> response = new HashMap<>();
+        response.put("message", "Delete Record!");
+        return new ResponseEntity<>(response, HttpStatus.OK);
+    }
 }
