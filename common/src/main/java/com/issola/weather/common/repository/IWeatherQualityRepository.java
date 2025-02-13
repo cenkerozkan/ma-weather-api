7d1ef8044b1fa428263ee0f6b9d4acca9d17dec5
cenkerozkan <ozkan.cenker@student.atilim.edu.tr>
2025-02-14 01:45:05 +0300

    New update method introduced in IWeatherQualityRepository.java for optimizing the service algorithm.

--- common/src/main/java/com/issola/weather/common/repository/IWeatherQualityRepository.java
+++ common/src/main/java/com/issola/weather/common/repository/IWeatherQualityRepository.java
@@ -1,7 +1,9 @@
 package com.issola.weather.common.repository;
 
+import com.issola.weather.common.dto.ResultsDto;
 import org.springframework.data.mongodb.repository.MongoRepository;
 import org.springframework.data.mongodb.repository.Query;
+import org.springframework.data.mongodb.repository.Update;
 import org.springframework.stereotype.Repository;
 
 import com.issola.weather.common.model.WeatherQuality;
@@ -12,6 +14,10 @@ import java.util.List;
 @Repository
 public interface IWeatherQualityRepository extends MongoRepository<WeatherQuality, String>
 {
-    @Query("{ 'city': ?0, 'results.date': { $gte: ?1, $lte: ?2 } }")
-    List<WeatherQuality> getWeatherQualityInRange(String city, LocalDate startDate, LocalDate endDate);
+    @Query("{ 'city': ?0}")
+    WeatherQuality getWeatherQualityByCity(String city);
+
+    @Query("{ 'city': ?0 }")
+    @Update("{ '$set': { 'results': ?1 }}")
+    void updateResultsByCity(String city, List<ResultsDto> results);
 }
